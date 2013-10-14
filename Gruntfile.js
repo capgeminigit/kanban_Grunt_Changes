88ee42bdb3ed6d2ab4d5cf99ee1dfbea24e3cd7a
capgeminigit <krish3693@gmail.com>
2013-10-14 18:58:39 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,10 @@
 module.exports = function(grunt){
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.registerTask('default', ['jshint']);
+
+    grunt.registerTask('default', ['sed']);
 
     grunt.loadNpmTasks('grunt-sed');
 
     grunt.initConfig({
-
-        jshint: {
-            options: {
-                expr: true,
-                es5: true,
-                curly: true,
-                eqeqeq: true,
-                newcap: true,
-                noempty: true,
-                nonew: true
-            },
-            globals: {},
-            all : ['Gruntfile.js', 'public/**/js']
-        },
-
         // get the list of tasks
         tasks: grunt.file.readJSON('./data/tasks.json'),
 
