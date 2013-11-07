41de7d5e3e7484cc96d3d740b66a75af9d7b5f26
capgeminigit <krish3693@gmail.com>
2013-11-07 12:58:08 +0530

    Update ctrl-dnd.js

--- public/js/app/ctrl-dnd.js
+++ public/js/app/ctrl-dnd.js
@@ -1,6 +1,6 @@
 function dndCtrl($scope) {
 
-	$scope.taskStore = [{"id":1,"name":"Fix IE9 bug","state":0,"nbrComments":0,"userId":1,"userName":"Hari Prasad","userImg":"img/mats.jpeg"},{"id":2,"name":"Sneak-install Chrome Frame","state":0,"nbrComments":1,"userId":1,"userName":"Mats","userImg":"img/mats.jpeg"},{"id":81,"name":"Sneak-install Chrome Frame1","state":0,"nbrComments":1,"userName":"","userImg":""},{"id":12,"name":"Add Windows Phone support","state":1,"nbrComments":1,"userId":3,"userName":"Brian","userImg":"img/brian.jpeg"},{"id":122,"name":"Make App","state":1,"nbrComments":1,"userName":"","userImg":""},{"id":3,"name":"Task 222 foo bar lots of text in this one eh? Fooooo","state":2,"nbrComments":0,"userName":"","userImg":"img/homer.jpg"},{"id":4,"name":"Find Unicorn","state":2,"nbrComments":0,"userName":"Homer","userId":2,"userImg":"img/homer.jpg"},{"id":5,"name":"IE6 support","state":1,"nbrComments":0,"userName":"","userImg":""},{"id":6,"name":"Chrome development","state":3,"nbrComments":0,"userName":"","userImg":"img/homer.jpg"},{"id":7,"name":"Find holy grail","state":3,"nbrComments":1,"userName":"","userImg":""},{"id":8,"name":"Dig hole","state":3,"nbrComments":0,"userName":"","userImg":""},{"id":9,"name":"Eat raisins","state":3,"nbrComments":3,"userName":"","userImg":"img/homer.jpg"}];
+	$scope.taskStore = TASKLIST;
 
 	$scope.dropListener = function(eDraggable, eDroppable) {
 
@@ -8,6 +8,7 @@ function dndCtrl($scope) {
 		var sSrc = eSrc.data('stid');
 
         if(sSrc == undefined){
+
             sSrc = eDraggable.data('stid');
             console.log('Dragging sourceID:' +sSrc);
         }
@@ -131,15 +132,7 @@ function dndCtrl($scope) {
 		}
 	}, true);
 
-    /*
-	$scope.taskStoreEmpty = function() {
-        console.log('length: '+ $scope.taskStore.length);
-		return $scope.taskStore.length == 0;
-
-	};
-    */
-
-	$scope.addTaskStoreEntry = function(state) {
+   	$scope.addTaskStoreEntry = function(state) {
 		$scope.addEntry($scope.taskStore, "changeUser", state);
 	};
 
