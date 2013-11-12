143cdae15a2c154f609afe5608fffdfd860648d2
capgeminigit <krish3693@gmail.com>
2013-11-12 11:47:56 +0530

    Update ctrl-dnd.js

--- public/js/app/ctrl-dnd.js
+++ public/js/app/ctrl-dnd.js
@@ -1,41 +1,128 @@
 function dndCtrl($scope) {
 
-	$scope.taskStore = TASKLIST;
+	$scope.taskStore = [ {
+		"id" : 1,
+		"name" : "Fix IE9 bug",
+		"state" : 0,
+		"nbrComments" : 0,
+		"userId" : 1,
+		"userName" : "Hari Prasad",
+		"userImg" : "img/mats.jpeg"
+	}, {
+		"id" : 2,
+		"name" : "Sneak-install Chrome Frame",
+		"state" : 0,
+		"nbrComments" : 1,
+		"userId" : 1,
+		"userName" : "Mats",
+		"userImg" : "img/mats.jpeg"
+	}, {
+		"id" : 81,
+		"name" : "Sneak-install Chrome Frame1",
+		"state" : 0,
+		"nbrComments" : 1,
+		"userName" : "",
+		"userImg" : ""
+	}, {
+		"id" : 12,
+		"name" : "Add Windows Phone support",
+		"state" : 1,
+		"nbrComments" : 1,
+		"userId" : 3,
+		"userName" : "Brian",
+		"userImg" : "img/brian.jpeg"
+	}, {
+		"id" : 122,
+		"name" : "Make App",
+		"state" : 1,
+		"nbrComments" : 1,
+		"userName" : "",
+		"userImg" : ""
+	}, {
+		"id" : 3,
+		"name" : "Task 222 foo bar lots of text in this one eh? Fooooo",
+		"state" : 2,
+		"nbrComments" : 0,
+		"userName" : "",
+		"userImg" : "img/homer.jpg"
+	}, {
+		"id" : 4,
+		"name" : "Find Unicorn",
+		"state" : 2,
+		"nbrComments" : 0,
+		"userName" : "Homer",
+		"userId" : 2,
+		"userImg" : "img/homer.jpg"
+	}, {
+		"id" : 5,
+		"name" : "IE6 support",
+		"state" : 1,
+		"nbrComments" : 0,
+		"userName" : "",
+		"userImg" : ""
+	}, {
+		"id" : 6,
+		"name" : "Chrome development",
+		"state" : 3,
+		"nbrComments" : 0,
+		"userName" : "",
+		"userImg" : "img/homer.jpg"
+	}, {
+		"id" : 7,
+		"name" : "Find holy grail",
+		"state" : 3,
+		"nbrComments" : 1,
+		"userName" : "",
+		"userImg" : ""
+	}, {
+		"id" : 8,
+		"name" : "Dig hole",
+		"state" : 3,
+		"nbrComments" : 0,
+		"userName" : "",
+		"userImg" : ""
+	}, {
+		"id" : 9,
+		"name" : "Eat raisins",
+		"state" : 3,
+		"nbrComments" : 3,
+		"userName" : "",
+		"userImg" : "img/homer.jpg"
+	} ];
 
 	$scope.dropListener = function(eDraggable, eDroppable) {
 
 		var eSrc = eDraggable.parent();
 		var sSrc = eSrc.data('stid');
-
-        if(sSrc == undefined){
-
-            sSrc = eDraggable.data('stid');
-            console.log('Dragging sourceID:' +sSrc);
-        }
+		if (sSrc == undefined) {
+			console.log('Added to test karma test case');
+			sSrc = eDraggable.data('stid');
+		}
+		console.log('Dragging sourceID' + sSrc);
 		var sTarget = eDroppable.data('stid');
-        console.log('Dropping sourceID:' +sTarget);
-
+		console.log('Dropping sourceID' + sTarget);
 		var modelSrc = "taskStore | filter:{ state: " + sSrc + "}";
 
-		$scope.$apply(function() {
-			var index = eDraggable.data('index');
-			var aSrc = $scope.$eval(modelSrc);
+		$scope
+				.$apply(function() {
+					var index = eDraggable.data('index');
+					var aSrc = $scope.$eval(modelSrc);
+					if (index == undefined) {
+						index = 0;
+					}
+					var item = aSrc[index];
+					if (item == undefined) {
+						index = 0;
+					}
+					if (sTarget == (Number(sSrc) + 1)
+							|| sTarget == (Number(sSrc) - 1)) {
+						if (item != undefined) {
+							item.state = sTarget;
+						}
 
-			var item = aSrc[index];
-            if (item == undefined){
-                index=0;
-                item = aSrc[index];
-            }
-
-			if (sTarget == (Number(sSrc) + 1)
-					|| sTarget == (Number(sSrc) - 1)) {
-
-                    if (item!=undefined){
-                        item.state = sTarget;
-                    }
-
-			}
-		});
+					} else {
+					}
+				});
 	};
 
 	function afterApply() {
@@ -109,7 +196,6 @@ function dndCtrl($scope) {
 		}
 	}, true);
 
-
 	$scope.$watch('test', function(newValue) {
 		if (angular.isArray(newValue)) {
 			newValue.map(function(e) {
@@ -119,7 +205,6 @@ function dndCtrl($scope) {
 			$scope.testCount = newValue.length;
 		}
 	}, true);
-
 
 	$scope.$watch('done', function(newValue) {
 		if (angular.isArray(newValue)) {
@@ -132,12 +217,18 @@ function dndCtrl($scope) {
 		}
 	}, true);
 
-   	$scope.addTaskStoreEntry = function(state) {
+	$scope.taskStoreEmpty = function() {
+		console.log('length: ' + $scope.taskStore.length);
+		return $scope.taskStore.length == 0;
+	};
+
+	$scope.addTaskStoreEntry = function(state) {
 		$scope.addEntry($scope.taskStore, "changeUser", state);
 	};
 
 	$scope.addEntry = function(items, imgIdPrefix, state) {
 		var id = Date.now();
+		console.log('items:' + items.length);
 		items.push({
 			"id" : id,
 			"name" : "New Task...",
@@ -150,6 +241,36 @@ function dndCtrl($scope) {
 		registerUserMenu(imgIdPrefix + id);
 
 	};
+
+	$scope.deleteTask = function(index, state) {
+		//alert('delete function called');
+		//console.log('index of the element ' + index);
+		// alert('Element Index >> '+index);
+		//alert('state of the element >> ' +state);
+
+		var modelSrc = "taskStore | filter:{ state: " + state + "}";
+		var aSrc = $scope.$eval(modelSrc);
+		//alert('model source length before remove ' +aSrc.length);
+		var id = aSrc[index].id;
+		var name = aSrc[index].name;
+		//alert('Id of the element >> ' +id);
+		//alert('Name of the element >> ' +name);
+		var items = $scope.taskStore;
+		var length = items.length;
+		//alert('length of the taskstore ' +length);
+		var index = 0;
+		for (i = 0; i < length; i++) {
+			index = i;
+			var idList = items[i].id;
+			if (id == idList) {
+				//alert('scope item id '+idList);
+				//alert('id and list in id matched');
+				//alert('index of item '+index);
+				$scope.taskStore.splice(index, 1);
+				break;
+			}
+
+		}
+
+	}
 }
-
-
